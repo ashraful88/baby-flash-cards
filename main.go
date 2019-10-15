86fddb531f360fae2579d72daaec8bf45900148a
ashraful <ashraful.abedein@mudah.my>
2019-10-15 18:17:51 +0800

    baby flash cards init

---
+++ main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"image"
+	"image/color"
+	"image/draw"
+	"image/png"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/golang/freetype"
+)
+
+var (
+	dpi      = flag.Float64("dpi", 300, "screen resolution in Dots Per Inch")
+	fontfile = flag.String("fontfile", "FZHTJW.TTF", "filename of the ttf font")
+	hinting  = flag.String("hinting", "none", "none | full")
+	size     = flag.Float64("size", 300, "font size in points")
+	spacing  = flag.Float64("spacing", 1.5, "line spacing (e.g. 2 means double spaced)")
+	wonb     = flag.Bool("whiteonblack", false, "white text on a black background")
+)
+
+var text = []string{
+	"A",
+}
+
+func makeFlashCard(cardFileName, text string) {
+	// Read the font data.
+	fontBytes, err := ioutil.ReadFile(*fontfile)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	f, err := freetype.ParseFont(fontBytes)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	// Initialize the context.
+	fg, bg := image.Black, image.White
+	ruler := color.RGBA{0xdd, 0xdd, 0xdd, 0xff}
+	if *wonb {
+		fg, bg = image.White, image.Black
+		ruler = color.RGBA{0x22, 0x22, 0x22, 0xff}
+	}
+	rgba := image.NewRGBA(image.Rect(0, 0, 1200, 1800))
+
+	draw.Draw(rgba, rgba.Bounds(), bg, image.ZP, draw.Src)
+	c := freetype.NewContext()
+	c.SetDPI(*dpi)
+	c.SetFont(f)
+	c.SetFontSize(*size)
+	c.SetClip(rgba.Bounds())
+	c.SetDst(rgba)
+	c.SetSrc(fg)
+	/* switch *hinting {
+	default:
+		c.SetHinting(font.HintingNone)
+	case "full":
+		c.SetHinting(font.HintingFull)
+	} */
+
+	// Draw the guidelines.
+	for i := 0; i < 200; i++ {
+		rgba.Set(10, 10+i, ruler)
+		rgba.Set(10+i, 10, ruler)
+	}
+
+	// Draw the text.
+	pt := freetype.Pt(170, 10+int(c.PointToFixed(*size)>>6))
+	_, err2 := c.DrawString(text, pt)
+	if err2 != nil {
+		log.Println(err2)
+		return
+	}
+	pt.Y += c.PointToFixed(*size * *spacing)
+
+	// Save that RGBA image to disk.
+	outFile, err := os.Create(cardFileName)
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	defer outFile.Close()
+	b := bufio.NewWriter(outFile)
+	err = png.Encode(b, rgba)
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	err = b.Flush()
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println("Wrote ", cardFileName)
+}
+
+func main() {
+	flag.Parse()
+	for i := 65; i <= 90; i++ {
+		makeFlashCard(fmt.Sprintf("./output/uc_%s.png", string(i)), string(i))
+	}
+	for i := 97; i <= 122; i++ {
+		makeFlashCard(fmt.Sprintf("./output/lc_%s.png", string(i)), string(i))
+	}
+}
